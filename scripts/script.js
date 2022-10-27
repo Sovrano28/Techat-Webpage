e336d5803278b8f1ebad5528153fc0ab1bbf85cf
Sovrano28 <Adeyemiayokanmi47@gmail.com>
2022-10-27 22:10:38 +0100

    started writing my custom js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const illusTextBreaktags = document.querySelector(".tbr");
+
+// if (window.matchMedia("(max-width: 991px) and (min-width: 768px)").matches) {
+//     illusTextBreaktags.remove();
+// } else {
+//     alert("non!");
+// };
